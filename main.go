e86c579ad61377b36a079124e8a7e85c38317021
Hei <xuboyu72@gmail.com>
2024-04-26 15:43:58 +0800

    👑 The basic and usable `goturbo` tool has been completed, with the version number assigned as `v0.1.0`.

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 var GoTurbo = &cobra.Command{
 	Use:           "goturbo",
-	Version:       "v0.0.6",
+	Version:       "v0.1.0",
 	Short:         "a toolkit designed to enhance the efficiency of Golang development",
 	SilenceUsage:  true,
 	SilenceErrors: true,
